e32ee60e974634d41a36cdebbdad1005c888ec14
Andrey <abramow0000@yandex.ru>
2020-09-02 14:54:24 +0300

    T - t (line 6)

--- models.py
+++ models.py
@@ -3,6 +3,6 @@ import sys
 
 app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QWidget()
-window.setWindowtitle("Первая программа на PyQT")
+window.setWindowTitle("Первая программа на PyQT")
 window.resize(300, 70)
 label = QtWidgets.QLabel("<center>Привет, мир!</center>")
